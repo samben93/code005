ef312cdf9c83dbbbaa938bfc4ea5f433d98a9cdd
samben93 <sam.jangce@gmail.com>
2021-08-20 22:17:26 +0800

    first commit

---
+++ src/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args){
+        int a=3;
+        int b=5;
+        int c=7;
+
+        int x=2;
+        int y=a*x + b*x*x + c*(x );
+
+        System.out.println(y);
+    }
+}
